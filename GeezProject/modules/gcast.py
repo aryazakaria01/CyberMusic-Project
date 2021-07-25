6547e07b6ade530d1c3bba45d016d4cb7508559a
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-07-25 15:52:39 +0700

    Update gcast.py

--- GeezProject/modules/gcast.py
+++ GeezProject/modules/gcast.py
@@ -25,7 +25,7 @@ async def bye(client, message):
     if message.from_user.id in SUDO_USERS:
         lol = await message.reply("`Globally Broadcasting Msg...`")
         if not message.reply_to_message:
-            await lol.edit("**Balas pesan teks apa pun untuk gcast**")
+            await lol.edit("**Reply others text for gcast**")
             return
         msg = message.reply_to_message.text
         sent=0
@@ -34,9 +34,9 @@ async def bye(client, message):
             try:
                 await client.send_message(dialog.chat.id, msg)
                 sent += 1
-                await lol.edit(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+                await lol.edit(f"**Succesfully Send Message to** `{sent}` **Group, Failed Sending Message to** `{failed}` **Grup**")
             except:
                 failed += 1
-                await lol.edit(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+                await lol.edit(f"**Succesfully Send Message to** `{sent}` **Group, Failed Sending Message to** `{failed}` **Grup**")
             await asyncio.sleep(0.7)
-        await message.reply_text(f"**Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+        await message.reply_text(f"**Sending Message to** `{sent}` **Group, Failed Sending Message to** `{failed}` **Grup**")
